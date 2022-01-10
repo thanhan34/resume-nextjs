9c894a277105b60e34439cb097bfd904fe261bb2
An Doan <dtan42@gmail.com>
2022-01-10 11:24:57 +1000

    improve the background for header on scroll

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,14 @@ import { useEffect, useState } from 'react'
 import HamburgerMenu from './HamburgerMenu'
 import { useRecoilState } from 'recoil'
 import { isOpenState } from '../Atom/isOpenAtom'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 function Header() {
     const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' })
     const [isOpen, setIsOpen] = useRecoilState(isOpenState)
-
+    const [hideOnScroll, setHideOnScroll] = useState(false)
+    useScrollPosition(({ prevPos, currPos }) => {
+        currPos.y > -100 ? setHideOnScroll(false) : setHideOnScroll(true)
+    }, [hideOnScroll])
     return (
         <>
             {isSmallScreen ?
@@ -22,7 +26,8 @@ function Header() {
                 )
                 :
                 (
-                    <nav className="sticky top-0 left-0 right-0 z-50 hidden max-w-3xl p-3 mx-auto text-white align-middle md:block">
+                    <nav
+                        className={`sticky top-0 left-0 right-0 z-50 hidden max-w-3xl p-3 mx-auto text-white align-middle ${hideOnScroll && "opacity-70 bg-slate-500 transition ease-in-out duration-100"}  md:block`}>
                         <ul className="flex flex-row items-center justify-center space-x-4">
                             <li><Link className="navItem" to="home" smooth={true} duration={1000} activeClass="active text-yellow-400" offset={-50} spy={true}>HOME</Link></li>
                             <li><Link className="navItem" to="about" smooth={true} duration={1000} activeClass="active text-yellow-400" spy={true}>ABOUT</Link></li>
@@ -32,6 +37,7 @@ function Header() {
                             <li><Link className="navItem" to="contact" smooth={true} duration={1000} activeClass="active text-yellow-400" spy={true}>CONTACT</Link></li>
                         </ul>
                     </nav>
+
                 )
             }
         </>
